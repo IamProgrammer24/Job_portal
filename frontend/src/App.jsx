9add1cd4868f2ee3870b0646c2fb910c43b42a62
Brajesh <281092322419e@gmail.com>
2025-05-13 23:41:45 +0530

    complete navbar ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Navbar from "./components/ui/shared/Navbar";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
