b1621d3acde32b4dbb95bda635fbc9091c81c885
Brajesh <281092322419e@gmail.com>
2025-05-14 19:28:39 +0530

    homepage ui complete

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,9 +8,9 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { USER_API_END_POINT } from "../utils/constant";
 import { toast } from "sonner";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setLoading } from "@/redux/authSlice";
-// import { Loader2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
 import Navbar from "../ui/shared/Navbar";
 
 const Signup = () => {
@@ -22,8 +22,8 @@ const Signup = () => {
     role: "",
     file: "",
   });
-  // const {loading,user} = useSelector(store=>store.auth);
-  // const dispatch = useDispatch();
+  const { loading } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const changeEventHandler = (e) => {
@@ -45,7 +45,7 @@ const Signup = () => {
     }
 
     try {
-      // dispatch(setLoading(true));
+      dispatch(setLoading(true));
       const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
         headers: { "Content-Type": "multipart/form-data" },
         withCredentials: true,
@@ -57,10 +57,9 @@ const Signup = () => {
     } catch (error) {
       console.log(error);
       toast.error(error.response.data.message);
+    } finally {
+      dispatch(setLoading(false));
     }
-    //  finally{
-    //     dispatch(setLoading(false));
-    // }
   };
 
   // useEffect(()=>{
@@ -152,7 +151,7 @@ const Signup = () => {
               />
             </div>
           </div>
-          {/* {loading ? (
+          {loading ? (
             <Button className="w-full my-4">
               {" "}
               <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait{" "}
@@ -161,10 +160,8 @@ const Signup = () => {
             <Button type="submit" className="w-full my-4">
               Signup
             </Button>
-          )} */}
-          <Button type="submit" className="w-full my-4">
-            Signup
-          </Button>
+          )}
+
           <span className="text-sm">
             Already have an account?{" "}
             <Link to="/login" className="text-blue-600">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 import { Toaster } from "@/components/ui/sonner";
 import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <Provider store={store}>
+      <App />
+      <Toaster /> {/* ✅ Now inside Redux Provider */}
+    </Provider>
   </StrictMode>
 );
