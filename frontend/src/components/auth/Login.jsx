52990df2dbffc3c426a1666bbb138f768eda3301
Brajesh <281092322419e@gmail.com>
2025-05-14 14:31:36 +0530

    login ui complete

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import Navbar from "../ui/shared/Navbar";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/Button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login page</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w7xl mx-auto ">
+        <form action="" className="w-1/2 border-gray-200 rounded-md p-4 my-10">
+          <h1 className="font-bold text-xl mb-5">Login</h1>
+          <div className="my-3">
+            <Label className="my-1">Email</Label>
+            <Input type="email" placeholder="Enter your email"></Input>
+          </div>
+          <div className="my-3">
+            <Label className="my-2">Password</Label>
+            <Input type="password" placeholder="Enter your Password"></Input>
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  classname="cursor-pointer"
+                ></Input>
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  classname="cursor-pointer"
+                ></Input>
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button type="submit" className="w-full my-4 ">
+            Login
+          </Button>
+          <span className="text-sm">
+            Already have an account?{" "}
+            <Link to="/signup" className="text-blue-600">
+              Sign Up
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
