24d1abcd0ea4924c718a1532152022aff2f9d41c
Brajesh <281092322419e@gmail.com>
2025-05-18 19:25:07 +0530

    implement filter jobs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import AdminJobs from "./components/admin/AdminJobs";
 import PostJob from "./components/admin/PostJob";
 import Applicants from "./components/admin/Applicants";
 import Browse from "./components/Browse";
+import ProtectedRoute from "./components/admin/ProtectedRoute";
 
 const appRouter = createBrowserRouter([
   {
@@ -46,27 +47,51 @@ const appRouter = createBrowserRouter([
   // admin's Routes
   {
     path: "/admin/companies",
-    element: <Companies />,
+    element: (
+      <ProtectedRoute>
+        <Companies />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/admin/companies/create",
-    element: <CompanyCreate />,
+    element: (
+      <ProtectedRoute>
+        <CompanyCreate />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/admin/companies/:id",
-    element: <CompanySetup />,
+    element: (
+      <ProtectedRoute>
+        <CompanySetup />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/admin/jobs",
-    element: <AdminJobs />,
+    element: (
+      <ProtectedRoute>
+        <AdminJobs />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/admin/jobs/create",
-    element: <PostJob />,
+    element: (
+      <ProtectedRoute>
+        <PostJob />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/admin/jobs/:id/applicants",
-    element: <Applicants />,
+    element: (
+      <ProtectedRoute>
+        <Applicants />
+      </ProtectedRoute>
+    ),
   },
 ]);
 
--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./ui/shared/Navbar";
 import FilterCard from "./FilterCard";
 import Job from "./Job";
@@ -7,7 +7,23 @@ import { useSelector } from "react-redux";
 // const jobs = [1, 2, 3, 4, 5, 6, 7, 9];
 
 const Jobs = () => {
-  const { allJobs } = useSelector((store) => store.job);
+  const { allJobs, searchedQuery } = useSelector((store) => store.job);
+  const [filterJobs, setFilterJobs] = useState(allJobs);
+
+  useEffect(() => {
+    if (searchedQuery) {
+      const filteredJobs = allJobs.filter((job) => {
+        return (
+          job.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+          job.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
+          job.location.toLowerCase().includes(searchedQuery.toLowerCase())
+        );
+      });
+      setFilterJobs(filteredJobs);
+    } else {
+      setFilterJobs(allJobs);
+    }
+  }, [allJobs, searchedQuery]);
 
   return (
     <div>
@@ -18,7 +34,7 @@ const Jobs = () => {
             <FilterCard />
           </div>
           <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
-            {allJobs.length === 0 ? (
+            {filterJobs.length === 0 ? (
               <span>Job not Found</span>
             ) : (
               <div className="grid grid-cols-3 gap-4">
